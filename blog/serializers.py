947c705a2bfa9b83ef21a121bed067fafdd0e81b
Mark Wolf <mark.wolf.music@gmail.com>
2014-06-08 21:01:12 -0400

    Improved translations of blog Post model data

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,8 +1,42 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from blog.models import Post
 
-class PostSerializer(ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
+
+    # Override Post fields to check for missing translations
+    title_en = serializers.SerializerMethodField('get_title_en')
+    text_en = serializers.SerializerMethodField('get_text_en')
+    title_zh_TW = serializers.SerializerMethodField('get_title_zh_TW')
+    text_zh_TW = serializers.SerializerMethodField('get_text_zh_TW')
+
+    # Serializer methods for filling in missing translations
+    def get_translation(self, obj, field, primary_lang, secondary_lang):
+        """
+        Helper method that returns the given field in the desired translation.
+        If the field is defined in the primary_lang it gives that or otherwise,
+        the secondary_lang.
+        """
+        primary_field = "{field}_{lang}".format(field=field, lang=primary_lang)
+        secondary_field = "{field}_{lang}".format(
+            field=field, lang=secondary_lang)
+        if getattr(obj, primary_field) == "":
+            string = getattr(obj, secondary_field)
+        else:
+            string = getattr(obj, primary_field)
+        return string
+
+    def get_title_en(self, obj):
+        return self.get_translation(obj, 'title', 'en', 'zh_TW')
+
+    def get_text_en(self, obj):
+        return self.get_translation(obj, 'text', 'en', 'zh_TW')
+
+    def get_title_zh_TW(self, obj):
+        return self.get_translation(obj, 'title', 'zh_TW', 'en')
+
+    def get_text_zh_TW(self, obj):
+        return self.get_translation(obj, 'text', 'zh_TW', 'en')
 
     class Meta:
         model = Post
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,57 @@
+# -*- coding: utf-8
 from django.test import TestCase
 
-# Create your tests here.
+from blog.models import Post
+from blog.serializers import PostSerializer
+
+class BlogPostTranslationsTest(TestCase):
+    """Make sure that the PostSerializer object operates correctly."""
+    def test_no_english_fields(self):
+        post = Post(
+            title_en="",
+            title_zh_TW=u"我去聖安東尼奧",
+            text_en="",
+            text_zh_TW=u"火山口酒廠來自美國西岸的奧勒岡州"
+        )
+        serializer = PostSerializer(post)
+        self.assertEqual(
+            serializer.data['title_en'],
+            post.title_zh_TW
+        )
+        self.assertEqual(
+            serializer.data['text_en'],
+            post.text_zh_TW
+        )
+        self.assertEqual(
+            serializer.data['title_zh_TW'],
+            post.title_zh_TW
+        )
+        self.assertEqual(
+            serializer.data['text_zh_TW'],
+            post.text_zh_TW
+        )
+
+    def test_no_chinese_fields(self):
+        post = Post(
+            title_en="Test blog post title",
+            title_zh_TW="",
+            text_en="I love my new blog post!",
+            text_zh_TW=""
+        )
+        serializer = PostSerializer(post)
+        self.assertEqual(
+            serializer.data['title_zh_TW'],
+            post.title_en
+        )
+        self.assertEqual(
+            serializer.data['text_zh_TW'],
+            post.text_en
+        )
+        self.assertEqual(
+            serializer.data['title_en'],
+            post.title_en
+        )
+        self.assertEqual(
+            serializer.data['text_en'],
+            post.text_en
+        )
